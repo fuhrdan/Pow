54422b0c72f54870cf348edc5cdf4e57ea809de0
fuhrdan <165685619+fuhrdan@users.noreply.github.com>
2024-06-25 08:02:32 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿//*****************************************************************************
+//** 50. Pow(x,n) leetcode                                                   **
+//** I started using loops and if statements.  That got me to 30X test cases **
+//** Then I moved to recursion for solving the problem. -Dan                 **
+//** P.S. - This was NOT a Hash table solution.                              **
+//*****************************************************************************
+
+double myPow(double x, long long n) {
+    if (n == 0)
+    {
+        return 1.0;
+    }
+    if (n < 0) 
+    {
+        long long john = -n;
+        long double triple = myPow(x, john);
+        return 1.0 / triple;
+    }
+    else
+    {
+        long double triple = myPow(x, n / 2);
+        return n % 2 == 0 ? triple * triple : triple * triple * x;
+    }
+}
